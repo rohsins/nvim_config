09d920214bfac9f113a10230aa8a26df8a90a6b7
Rohit Singh <rohsins@gmail.com>
2025-09-02 08:44:56 +0545

    add floterm plugin and change options

--- lua/r/option.lua
+++ lua/r/option.lua
@@ -1,11 +1,15 @@
-local cmd = vim.cmd
+-- local cmd = vim.cmd
 local opt = vim.opt
 local api = vim.api
 
 -- opt.rtp:append(vim.fn.stdpath "config" .. "/lua/r/after")
 -- print(vim.fn.stdpath "config" .. "/lua/r/after")
 
+opt.tabstop = 4
+opt.softtabstop = 4
 opt.shiftwidth = 4
+opt.expandtab = false
+opt.updatetime = 50
 
 opt.relativenumber = true
 opt.number = true
@@ -15,15 +19,22 @@ opt.autoindent = true
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
 
 opt.cursorline = true
-opt.termguicolors = false
+opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
+opt.scrolloff = 8
 
 opt.clipboard:append("unnamedplus")
 
 opt.showmode = false
+opt.wrap = false
+
+opt.swapfile = false
+opt.backup = false
 
 api.nvim_create_autocmd('TextYankPost', {
 	desc = "Highlight when yanking (copying) text",
---
+++ lua/r/plugin/floterm.lua
@@ -0,0 +1,9 @@
+return {
+    -- "rohsins/nvim-floterm",
+    dir="~/Software/Projects/nvim-floterm/",
+    name="nvim-floterm",
+    dev=true,
+    opts = {
+    },
+}
+
